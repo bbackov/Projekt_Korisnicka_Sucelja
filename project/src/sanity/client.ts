fc59d52c0a0810cee464d8e08c4a4189ed0241f8
Rene~ <renat.caric123@gmail.com~>
2026-02-24 18:00:16 +0100

    Sanity config

--- project/src/sanity/client.ts
+++ project/src/sanity/client.ts
@@ -4,12 +4,13 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01";
 
-if (!projectId) throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID");
-if (!dataset) throw new Error("Missing NEXT_PUBLIC_SANITY_DATASET");
+export const isSanityConfigured = Boolean(projectId && dataset);
 
-export const sanityClient = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true,
-});
+export const sanityClient = isSanityConfigured
+  ? createClient({
+      projectId,
+      dataset,
+      apiVersion,
+      useCdn: true,
+    })
+  : null;
