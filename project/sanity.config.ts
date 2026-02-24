82e774f1c0dc4178e1997aa65fd3c77ca1c4bd9e
Rene~ <renat.caric123@gmail.com~>
2026-02-24 19:05:29 +0100

    Add Sanity Studio config for Next app

---
+++ project/sanity.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { schemaTypes } from "./sanity/schemaTypes";
+
+export default defineConfig({
+  name: "default",
+  title: "MatchTrack CMS",
+  projectId: "1pw77ro4",
+  dataset: "production",
+  plugins: [deskTool()],
+  schema: { types: schemaTypes },
+});
